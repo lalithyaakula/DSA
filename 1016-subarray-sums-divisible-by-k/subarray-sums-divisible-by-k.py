0d8ed3de9a9f4db14f6c55735c4bcc33680397ee
lalithya <lalithyaakula@gmail.com>
2026-03-10 18:11:47 +0530

    Time: 39 ms (20.43%) | Memory: 23.6 MB (42.52%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        hash_map={0:1}
+        count=0
+        ans=0
+        for i in range(0,len(nums)):
+            ans+=nums[i]
+            mod=ans%k
+            if mod<0:
+                mod+=k
+            if mod in hash_map:
+                count+=hash_map[mod]
+                hash_map[mod]+=1
+            else:
+                hash_map[mod]=1
+        return count
+
+
+
+        
