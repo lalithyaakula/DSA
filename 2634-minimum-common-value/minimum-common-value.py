7e9fe4ae0029e4b7132d8a3d498c39ea358bc6c4
lalithya <lalithyaakula@gmail.com>
2026-05-19 08:27:11 +0530

    Time: 7 ms (85.33%) | Memory: 42.8 MB (13.52%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        s=set(nums1)
+        for num in nums2:
+            if num in s:
+                return num
+                break
+        return -1
