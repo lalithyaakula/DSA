a49077e8b4640ebb761b131757896dd47ccacf5b
lalithya <lalithyaakula@gmail.com>
2026-08-03 20:04:47 +0530

    Time: 296 ms (76.57%) | Memory: 51.6 MB (68.01%) - LeetSync

---
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.py
@@ -0,0 +1,17 @@
+class TreeAncestor:
+
+    def __init__(self, n: int, parent: List[int]):
+        m = 1 + int(log2(n)) #at most 16 for this problem 
+        self.dp = [[-1] * m for _ in range(n)] #ith node's 2^j parent
+        for j in range(m):
+            for i in range(n):
+                if j == 0: self.dp[i][0] = parent[i] #2^0 parent
+                elif self.dp[i][j-1] != -1: 
+                    self.dp[i][j] = self.dp[self.dp[i][j-1]][j-1]
+    
+    def getKthAncestor(self, node: int, k: int) -> int:
+        while k > 0 and node != -1: 
+            i = int(log2(k))
+            node = self.dp[node][i]
+            k -= (1 << i)
+        return node 
